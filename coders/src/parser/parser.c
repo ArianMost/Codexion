68dff1e3012d777b739a321659155eeb257027fa
Arian Mostashari <53341622+ArianMost@users.noreply.github.com>
2026-07-29 18:11:52 +0200

    Fix usage message and update argument parsing

--- coders/src/parser/parser.c
+++ coders/src/parser/parser.c
@@ -6,12 +6,26 @@
 /*   By: amostash <amostash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 18:58:07 by amostash          #+#    #+#             */
-/*   Updated: 2026/07/22 18:58:10 by amostash         ###   ########.fr       */
+/*   Updated: 2026/07/29 17:59:41 by amostash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+static int	parse_scheduler(const char *s, int *res)
+{
+	if (strcmp(s, "fifo") == 0)
+	{
+		*res = SCHED_FIFO_MODE;
+		return (1);
+	}
+	else if (strcmp(s, "edf") == 0)
+	{
+		*res = SCHED_EDF_MODE;
+		return (1);
+	}
+	return (0);
+}
 
 static int	parse_number(const char *s, long long *res)
 {
@@ -20,10 +34,8 @@ static int	parse_number(const char *s, long long *res)
 
 	i = 0;
 	number = 0;
-
 	if (s == NULL || s[0] == '\0')
 		return (0);
-
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -34,29 +46,30 @@ static int	parse_number(const char *s, long long *res)
 		i++;
 	}
 	*res = number;
-
 	return (1);
 }
 
+static int	print_usage(char *prog)
+{
+	fprintf(stderr,
+		"Right usage: %s number_of_coders time_to_burnout "
+		"time_to_compile time_to_debug time_to_refactor "
+		"number_of_compiles_required dongle_cooldown scheduler\n",
+		prog);
+	return (0);
+}
 
 int	parse_args(int argc, char **argv, t_config *config)
 {
 	long long	number_of_coders;
 
 	if (argc != 9)
-	{
-		fprintf(stderr, "Right usage: %s number_of_coders time_to_burnout "
-			"time_to_compile time_to_debug time_to_refactor "
-			"number_of_compiles_required dongle_cooldown scheduler\n",
-			argv[0]);
-		return (0);
-	}
-
+		return (print_usage(argv[0]));
 	if (!parse_number(argv[1], &number_of_coders) || number_of_coders < 1)
 		return (fprintf(stderr, "invalid number_of_coders\n"), 0);
-
 	config->number_of_coders = (int)number_of_coders;
-	if (!parse_number(argv[2], &config->time_to_burnout) || config->time_to_burnout < 1)
+	if (!parse_number(argv[2], &config->time_to_burnout)
+		|| config->time_to_burnout < 1)
 		return (fprintf(stderr, "invalid time_to_burnout\n"), 0);
 	if (!parse_number(argv[3], &config->time_to_compile))
 		return (fprintf(stderr, "invalid time_to_compile\n"), 0);
@@ -70,6 +83,5 @@ int	parse_args(int argc, char **argv, t_config *config)
 		return (fprintf(stderr, "invalid dongle_cooldown\n"), 0);
 	if (!parse_scheduler(argv[8], &config->scheduler))
 		return (fprintf(stderr, "scheduler must be 'fifo' or 'edf'\n"), 0);
-
 	return (1);
 }
