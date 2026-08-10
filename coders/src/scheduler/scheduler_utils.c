e5d88995be70a1febba96de4f3472f2ceff6a033
Arian Mostashari <53341622+ArianMost@users.noreply.github.com>
2026-08-10 14:42:01 +0200

    Enhance priority comparison with scheduler mode
    Modified the higher_priority function to include scheduler mode handling. Updated calls to higher_priority to pass the scheduler parameter.

--- coders/src/scheduler/scheduler_utils.c
+++ coders/src/scheduler/scheduler_utils.c
@@ -6,16 +6,18 @@
 /*   By: amostash <amostash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/24 19:18:55 by amostash          #+#    #+#             */
-/*   Updated: 2026/07/27 16:53:28 by amostash         ###   ########.fr       */
+/*   Updated: 2026/08/01 16:31:51 by amostash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-static int	higher_priority(t_heap_node *a, t_heap_node *b)
+static int	higher_priority(t_heap_node *a, t_heap_node *b, int scheduler)
 {
 	if (a->key_sort != b->key_sort)
 		return (b->key_sort > a->key_sort);
+	if (scheduler == SCHED_EDF_MODE)
+		return (a->coder_id > b->coder_id);
 	return (b->proirity_seq > a->proirity_seq);
 }
 
@@ -35,7 +37,7 @@ void	bubble_up(t_heap *queue, int i)
 	while (i > 0)
 	{
 		parent = (i - 1) / 2;
-		if (!higher_priority(&queue->requests[i], &queue->requests[parent]))
+		if (!higher_priority(&queue->requests[i], &queue->requests[parent], queue->scheduler))
 			break ;
 		swap_nodes(&queue->requests[i], &queue->requests[parent]);
 		i = parent;
@@ -55,11 +57,11 @@ void	bubble_down(t_heap *queue, int i)
 		right = 2 * i + 2;
 		if (left < queue->used_capacity
 			&& higher_priority(&queue->requests[left],
-				&queue->requests[smallest]))
+				&queue->requests[smallest], queue->scheduler))
 			smallest = left;
 		if (right < queue->used_capacity
 			&& higher_priority(&queue->requests[right],
-				&queue->requests[smallest]))
+				&queue->requests[smallest], queue->scheduler))
 			smallest = right;
 		if (smallest == i)
 			break ;
